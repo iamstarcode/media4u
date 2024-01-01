2300519c5760e4209af123ac043668b177a8eca1
Bakare Abiola <iamstarcode@gmail.com>
2024-01-02 00:05:07 +0100

    chore: rc 2

--- apps/anim4u/src/cli.ts
+++ apps/anim4u/src/cli.ts
@@ -2,9 +2,13 @@
 import { Command } from 'commander';
 import { CLI } from '@iamstarcode/4u-lib';
 
+import fs from 'fs';
+const packagejson = JSON.parse(fs.readFileSync('./package.json', 'utf8'));
+
 (async () => {
   const program = new Command();
-  const version = process.env.npm_package_version;
+  const version = packagejson.version;
+  //console.log(packagejson.version);
 
   await CLI.handleIfNewVersion(version!, 'anim4u');
 
